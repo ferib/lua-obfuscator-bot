19d93bfc85b5db0cb2f03c2f7eed8b5ee30e1a93
ferib <admin@ferib.be>
2023-03-03 23:21:08 +0100

    working obf

--- bot.lua
+++ bot.lua
@@ -8,6 +8,34 @@ local client = discordia.Client()
 local adminId = 244846359763615744
 local HTTP_PATH = "https://luaobfuscator.com/api/obfuscator/"
 
+-- helpers
+
+local function parsePre(s)
+	local res = ""
+  	
+	-- find ` if any
+	local sx, sy = s:find("```")
+	if sx == nil then
+		return nin
+	end
+	res = s:sub(sy+1, #s)
+
+	-- take next line
+	local nx, ny = res:find("\n")
+	if nx == nil then
+		return nil
+	end
+	res = res:sub(ny+1, #s)
+	
+	-- take until end
+	local ex, ey = res:find("```")
+	if ex == nil then
+		return nil
+	end
+	res = res:sub(1, ex-1)
+	return res
+end
+
 
 client:on("ready", function()
 	print("Logged in as ".. client.user.username)
@@ -33,7 +61,7 @@ client:on("messageCreate", function(message)
 		(message.content:sub(1, #cmd) == cmd or
 		message.content:sub(1, #cmd2) == cmd2) then
 
-		--message.channel:send(message.content)
+		--print(dump(message.content))
 		print("Obfuscation request by: " .. message.author.username)	
 		
 		-- check for files/content one way or another
@@ -46,10 +74,14 @@ client:on("messageCreate", function(message)
 				return
 			end
 			msg = body
-		--elseif message.embeds ~= nil and #message.embeds > 0 then
-		--	pp.print(dump(message.embeds))	
+		else --if message.embeds ~= nil and #message.embeds > 0 then
+			local parsedMsg = parsePre(message.content)
+			if parsedMsg ~= nil then
+				msg = parsedMsg
+			end
 		end
-		if message.attachments == nil or #message.attachments == 0 then
+
+		if msg == nil then
 			message.channel:send("Please provide a valid Lua 5.1 script _(partial support for Lua 5.4.3 is available)_")
 			return
 		end
@@ -66,7 +98,7 @@ client:on("messageCreate", function(message)
 		
 		-- upload to create a new session
 		local res, body = http.request("POST", 
-			HTTP_PATH .. "newscript", 
+			HTTP_PATH .. "one-click/hard", -- "newscript", 
 			{
 				{ "Content-Type", "application/json" },
 				{ "apikey", "test" }
@@ -75,10 +107,14 @@ client:on("messageCreate", function(message)
 		
 		-- TODO: call a pre-defined 1-click button obfuscation?	
 
-		print(dump(options))
 		local data = json.parse(body)
-		local result = "https://luaobfuscator.com/?session=" .. data.sessionId
-		print(dump(res))
+		if data == nil then
+			message.channel:send("Error during obfuscation!")
+			return
+		end
+		local result = data.code --"https://luaobfuscator.com/?session=" .. data.sessionId
+		local url = "https://luaobfuscator.com/?session=" .. data.sessionId
+		--print(dump(data))
 		
 		-- handle HTTP errors
 		if res.code ~= 200 then
@@ -93,7 +129,10 @@ client:on("messageCreate", function(message)
 		end
 
 		-- OK?
-		message.channel:send(result)
+		message.channel:send {
+			content = "Obfuscation complete _(tmp link <" .. url .. ">)_",
+			file = { "luaObfuscated.lua", result }
+		}
 	end
 end)
 
@@ -101,6 +140,5 @@ end)
 -- get key
 local key = fs.readFileSync("key.dat")
 key = key:sub(1, #key-1) -- stupid line ending I can't seem to get rid of??
-
 client:run(key)
 
